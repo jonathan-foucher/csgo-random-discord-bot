adc1a29a7bf3ab5af374beba761e61749dec9c68
Jonathan Foucher <jonathan-foucher@outlook.fr>
2021-12-29 20:04:38 +0100

    rework files

--- README.md
+++ README.md
@@ -1,31 +1,27 @@
 # Overview
-This project is about creating a discord bot that generates a random combination of weapon and pistol for the game Counter Strike: Global Offensive.
-It's kind of a spin wheel to determine what weapons the player should use during the next match for a challenge/fun purpose.
+This project is about creating a discord bot for fun *Counter Strike: Global Offensive*.
 
+### CSGO Random Weapon
+Generates a random combination of weapon and pistol for the game Counter Strike: Global Offensive.
+It's kind of a spin wheel to determine what weapons the player should use during the next match for a challenge/fun purpose.
 
 Some weapons are only available on a side (ct or t) then we decided to regroup them as the game did.
 
-# How to install the project 
+# Install the project 
 Install Python 3 and the next libraries :
-```
-pip3 install discord
-```
-```
-pip3 install python-dotenv
-```
-```
-pip3 install Pillow
-```
+<br>`pip3 install discord`
+<br>`pip3 install python-dotenv`
+<br>`pip3 install Pillow`
 
 Create .env file in the ./src directory with your Discord bot token:
-```
-CRWD_BOT_TOKEN=XXXX
-```
-# How to run the project
+<br>`CRWD_BOT_TOKEN=XXXX`
+
+# Start the bot
 Create a bot on Discord and invite it on your server with the appropriate permissions.
 
 Then simply run the main.js file on your server.
 
+# Bot usage
 To get a random combination of weapon and pistol use the command `/crw`, the bot need to have the permissions to read and write on the channel.
 It also works on private message directly with the bot.
 
@@ -35,7 +31,7 @@ The `-p` option allows you to specify from 1 to 5 players name.
 <br>If not specified, the default will be the nickname or the name of the player using the command. 
 <br>Exemple : `/crw -p Alex John`
 
-For each player of the command to get a unique primary weapon, you have to use the `-u` option.
+For each player to get a unique primary weapon, you have to use the `-u` option.
 <br>By default, the players can get the same primary weapon. 
 <br>Exemple : `/crw -u -p Alex John`
 
--- src/weapon.py
+++ src/weapon.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import os
 from collections import defaultdict
@@ -5,6 +6,8 @@ from io import BytesIO
 
 import discord
 from PIL import Image
+
+from utils import get_random_element
 
 script_dir = os.path.dirname(__file__)
 
@@ -21,8 +24,9 @@ class Weapon:
 
 
 def get_weapons_image(weapons_list):
-    images = [Image.open(os.path.join(script_dir, '../resources/img/weapons/{}.png'.format(weapon['name'].replace(' ', '_'))))
-              for weapon in weapons_list]
+    images = [
+        Image.open(os.path.join(script_dir, '../resources/img/weapons/{}.png'.format(weapon['name'].replace(' ', '_'))))
+        for weapon in weapons_list]
     width, height = tuple(s // 4 for s in images[0].size)
     images = [i.resize((width, height), Image.ANTIALIAS) for i in images]
 
@@ -51,3 +55,61 @@ def generate_weapons_list(weapons_type):
         wpn_image = get_weapons_image(wpn_group)
         weapons_list.append(Weapon(wpn_name, wpn_image))
     return weapons_list
+
+
+def manage_crw_options(options_str):
+    players = list()
+    is_unique_weapons = False
+    try_help_message = '\nTry `/crw -h` for more information'
+
+    if options_str and not options_str.startswith('-'):
+        return None, None, 'Error: Unexpected argument {}{}'.format(options_str.split(' ')[0], try_help_message)
+
+    options = list(filter(None, options_str.split('-')))
+    while options:
+        args = list(filter(None, options[0].split(' ')))
+        option = args[0]
+        args.pop(0)
+        if option == 'h':
+            return None, None, \
+                   'CSGO random weapons generator' \
+                   '\nUsage: `/crw [-u] [-p] [PLAYER]`' \
+                   '\n\nOptions:' \
+                   '\n-u                 Generate a unique primary weapon for each player (default is not unique).' \
+                   '\n-p string     Generate with a player list, from 1 to 5 player names separated by a space.' \
+                   ' By default it will use the nickname or name of the user calling the command.'
+        elif option == 'p':
+            if args:
+                players = list(args)
+            else:
+                return None, None, 'Error: -p option expects from 1 to 5 player names separated by a single space'
+        elif option == 'u':
+            if args:
+                return None, None, 'Error: -u option does not accept arguments'
+            else:
+                is_unique_weapons = True
+        else:
+            return None, None, 'Error: -{} option is unknown'.format(option, try_help_message)
+        options.pop(0)
+    if players and len(players) > 5:
+        return None, None, 'Error: -p option accepts a maximum of 5 player names'
+    return players, is_unique_weapons, None
+
+
+async def send_random_weapon(channel, players, is_unique_weapons):
+    pistols_copy = copy.deepcopy(pistols)
+    weapons_copy = copy.deepcopy(weapons)
+
+    for player in players:
+        random_pistol = get_random_element(pistols_copy)
+        random_weapon = get_random_element(weapons_copy)
+
+        response = '{} weapons are : {} and {}'.format(player, random_pistol.name, random_weapon.name)
+        images = [copy.deepcopy(random_pistol.image), copy.deepcopy(random_weapon.image)]
+        await channel.send(response, files=images)
+        if is_unique_weapons:
+            weapons_copy.remove(random_weapon)
+
+
+pistols = generate_weapons_list('pistols')
+weapons = generate_weapons_list('weapons')
