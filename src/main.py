08664566c933e7c9bb5da4341788b647f1c2951d
Jonathan Foucher <jonathan-foucher@outlook.fr>
2021-12-31 06:48:31 +0100

    add options on crm command to get multiple maps and filter bomb objective maps

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import os
 import discord
 from dotenv import load_dotenv
 
-from map import send_random_map
+from map import manage_crm_options, send_random_map
 from weapon import manage_crw_options, send_random_weapon
 
 client = discord.Client()
@@ -31,12 +31,17 @@ async def on_message(message):
             await send_random_weapon(message.channel, players, is_unique_weapons)
 
     if message.content == '/crm' or message.content.startswith('/crm '):
-        await send_random_map(message.channel)
+        number_of_maps, is_bomb_map_only, message_to_send = manage_crm_options(message.content[5:])
+        if message_to_send:
+            await message.channel.send(message_to_send)
+        else:
+            await send_random_map(message.channel, number_of_maps, is_bomb_map_only)
 
     if message.content == '/help' or message.content.startswith('/help '):
         help_message = 'CSGO random bot' \
                        '\n\nCommands:' \
                        '\n/crw    Generate a random combination of primary weapon and pistol.' \
+                       '\n/crm    Get random maps.' \
                        '\n\nYou can use `COMMAND -h` to get the help documentation of a command.'
         await message.channel.send(help_message)
 
